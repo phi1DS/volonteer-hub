7127f11fb8e7c1435498c9669cabe31674dee20d
Philippe Ds <phil.deiss@gmail.com>
2026-01-08 13:33:12 +0100

    refactor e2e seyup so it uses env vars

--- e2e/auth.setup.ts
+++ e2e/auth.setup.ts
@@ -1,11 +1,10 @@
 import { test as setup, expect } from '@playwright/test';
 import path from 'path';
-import { fileURLToPath } from 'url';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-const authFile = path.resolve(__dirname, '../playwright/.auth/user.json');
+const authFile = path.resolve(
+  import.meta.dirname,
+  '../playwright/.auth/user.json'
+);
 
 setup('authenticate', async ({ request }) => {
   const response = await request.get('/testing/login');
