f6fd0cb280f70a4bc6e5bba3414395ddaf60f934
Philippe Ds <phil.deiss@gmail.com>
2026-01-09 15:54:48 +0100

    e2e specify data for playwright

--- e2e/backoffice.spec.ts
+++ e2e/backoffice.spec.ts
@@ -4,12 +4,12 @@ import { createTask as dbHelperCreateTask, pruneDB as databaseHelperPruneDB } fr
 
 test.describe('Backoffice page tests', () => {
 
-  // test.beforeEach(async ({ request: apiRequest }) => {
-  //   await databaseHelperPruneDB(apiRequest);
-  // });
+  test.afterEach(async ({ request: apiRequest }) => {
+    await databaseHelperPruneDB(apiRequest);
+  });
 
-  test('User sees task AC1: Task shows up on task page', async ({ page, request: apiRequest }) => {
-    const taskSubject = 'Help with cleaning dishes';
+  test('User sees task AC1: Task shows up on task page', async ({ page, request: apiRequest }, testInfo) => {
+    const taskSubject = `Help with cleaning dishes ${testInfo.project.name}`;
     await dbHelperCreateTask(apiRequest, { subject: taskSubject, user_id: 1 });
 
     await page.goto('/dashboard');
@@ -17,31 +17,28 @@ test.describe('Backoffice page tests', () => {
     await expect(page.getByText(taskSubject)).toBeVisible();
   });
 
-  test('User create task AC1: Successfully create a task', async ({ page }) => {
+  test('User create task AC1: Successfully create a task', async ({ page }, testInfo) => {
     await page.goto('/dashboard');
 
-    // Form filling
     const startDate = format(addYears(new Date(), 1), "yyyy-MM-dd'T'HH:mm");
     const endDate = format(addYears(new Date(), 2), "yyyy-MM-dd'T'HH:mm");
+    const taskSubject = `Help with oranges ${testInfo.project.name}`;
 
     await page.getByRole('button', { name: 'Create New Task' }).click();
-    await page.getByRole('textbox', { name: 'Sujet *' }).fill('Help with oranges');
+    await page.getByRole('textbox', { name: 'Sujet *' }).fill(taskSubject);
     await page.getByRole('textbox', { name: 'Message *' }).fill('My Message');
     await page.getByRole('textbox', { name: 'Organisation' }).fill('My Organisation');
     await page.getByRole('textbox', { name: 'Contact information *' }).fill('06 06 76 87 98');
     await page.locator('input[name="date_start"]').fill(startDate);
     await page.locator('input[name="date_end"]').fill(endDate);
 
-    // Submitting
     await page.getByRole('button', { name: 'Create Task' }).click();
 
-    // Expectations
-    await expect(page.getByRole('listitem').filter({ hasText: 'Task created' })).toBeVisible();
-    await expect(page.getByText('Help with oranges')).toBeVisible();
+    await expect(page.getByText(taskSubject)).toBeVisible();
   });
 
-  test('User close Task AC1: Successfully close task', async ({ page, request: apiRequest }) => {
-    const taskSubject = 'Help with furniture';
+  test('User close Task AC1: Successfully close task', async ({ page, request: apiRequest }, testInfo) => {
+    const taskSubject = `Help with furniture ${testInfo.project.name}`;
     await dbHelperCreateTask(apiRequest, { subject: taskSubject, user_id: 1 });
 
     await page.goto('/dashboard');
@@ -55,12 +52,11 @@ test.describe('Backoffice page tests', () => {
     await taskCard.getByRole('button', { name: 'Close Task' }).click();
     await page.locator('[data-slot="dialog-content"]').getByRole('button', { name: 'Close task' }).click();
 
-    await expect(page.getByText('Task marked as closed')).toBeVisible();
     await expect(taskCard).not.toBeVisible();
   });
 
-  test('User update Task AC1: Successfully update task', async ({ page, request: apiRequest }) => {
-    const taskSubject = 'Help with groceries';
+  test('User update Task AC1: Successfully update task', async ({ page, request: apiRequest }, testInfo) => {
+    const taskSubject = `Help with groceries ${testInfo.project.name}`;
     await dbHelperCreateTask(apiRequest, { subject: taskSubject, user_id: 1 });
     
     await page.goto('/dashboard');
@@ -69,11 +65,11 @@ test.describe('Backoffice page tests', () => {
       has: page.getByText(taskSubject),
     });
 
+    const newTaskSubject = `Help with fruits ${testInfo.project.name}`;
     await taskCard.getByRole('button', { name: 'Update' }).click();
-    await page.getByRole('textbox', { name: 'Sujet *' }).fill('Help with fruits');
+    await page.getByRole('textbox', { name: 'Sujet *' }).fill(newTaskSubject);
     await page.getByRole('button', { name: 'Update Task' }).click();
 
-    await expect(page.getByText('Task updated')).toBeVisible();
-    await expect(page.getByText('Help with fruits')).toBeVisible();
+    await expect(page.getByText(newTaskSubject)).toBeVisible();
   });
 });
--- playwright.config.ts
+++ playwright.config.ts
@@ -17,9 +17,9 @@ export default defineConfig({
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
+  retries: 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  workers: 1, // concurrencies breaks data state in tests, keep 1
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
@@ -40,7 +40,6 @@ export default defineConfig({
       testMatch: /.*\.spec\.ts/,
       use: {
         ...devices['Desktop Chrome'],
-        // Use prepared auth state.
         storageState: 'playwright/.auth/user.json',
       },
       dependencies: ['setup'],
@@ -50,7 +49,6 @@ export default defineConfig({
       name: 'firefox',
       use: {
         ...devices['Desktop Firefox'],
-        // Use prepared auth state.
         storageState: 'playwright/.auth/user.json',
       },
       dependencies: ['setup'],
